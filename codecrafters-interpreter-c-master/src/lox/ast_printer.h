11fdc8417f440f39fe5b343fb0bedbfa2f792cb9
chuegue <reffranciscosimplicio@gmail.com>
2025-05-04 17:01:19 +0100

    .gitignore, make, readme.md

--- codecrafters-interpreter-c-master/src/lox/ast_printer.h
+++
@@ -1,8 +0,0 @@
-#ifndef CODECRAFTERS_INTERPRETER_LOX_AST_PRINTER_H
-#define CODECRAFTERS_INTERPRETER_LOX_AST_PRINTER_H
-
-#include "lox/expr.h"
-
-void println_expr(const Expr *expr);
-
-#endif
--- codecrafters-interpreter-c-master/src/lox/resolver.h
+++
@@ -1,8 +0,0 @@
-#ifndef CODECRAFTERS_INTERPRETER_LOX_RESOLVER_H
-#define CODECRAFTERS_INTERPRETER_LOX_RESOLVER_H
-
-#include "util/vector.h"
-
-void resolve_stmts(const Vector *statements);
-
-#endif
